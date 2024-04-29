e2c88e51cb260ea422efdce58eee0b24623273c6
leventium <leventium@users.noreply.github.com>
2024-04-29 21:01:49 +0300

    Composition of control unit blocks in unite module

--- src/sr_control.v
+++ src/sr_control.v
@@ -1,48 +1,138 @@
 `include "sr_cpu.vh"
 
-`define DM_BYTE     3'b001
-`define DM_HALF     3'b010
-`define DM_WORD     3'b100
 
-`define SELECT_COMB 1'b0;
-`define SELECT_CRYP 1'b1;
+module sr_control (
+    input              clk,
+    input              rst_n,
 
+    input      [ 6:0]  cmdOp,
+    input      [ 2:0]  cmdF3,
+    input      [ 6:0]  cmdF7,
+    input      [ 4:0]  cmdF5_rs2,
+    input              aluZero,
 
-module sr_control
-(
-    input      [ 6:0] cmdOp,
-    input      [ 2:0] cmdF3,
-    input      [ 6:0] cmdF7,
-    input      [ 4:0] cmdF5_rs2,
-    input             aluZero,
-
-    output reg        hold,
-    // output           pcSrc,     // pcBranch : pcPlus4
-    output reg        regWrite,  // rf write enable
-    // output reg       aluSrc,    // immediate : rd2
-    // output reg       wdSrc,     // immU : execResult
-    // output reg       immPick,   // immS : immI
-    // output reg       memToReg,  // dmDataR : aluResult
+    output             hold,
+    output reg         regWrite,
 
     // multiplexers
-    output reg        aluSrc1,
-    output reg  [1:0] aluSrc2,
-    output reg  [3:0] aluControl,
-    output reg  [1:0] pcSrc1,
-    output reg        pcSrc2,
-    output reg  [2:0] wdSrc,
+    output             aluSrc1,
+    output      [1:0]  aluSrc2,
+    output      [3:0]  aluControl,
+    output reg  [1:0]  pcSrc1,
+    output reg         pcSrc2,
+    output reg  [2:0]  wdSrc,
 
     // memory
-    output reg        dmWe,      // data memory write enable
-    output reg        dmSign,    // data memory signed read
-    output            dmOpByte,   // data memory operation mode
-    output            dmOpHalf,
-    output            dmOpWord,
+    output reg         dmWe,
+    output             dmSign,
+    output             dmOpByte,
+    output             dmOpHalf,
+    output             dmOpWord,
 
     // crypto module
-    // output reg        cry_i_valid,
-    output reg [20:0] cryptoMode
+    output     [20:0] cryptMode
 );
+
+    wire        regWrite_comb;
+    wire [1:0]  pcSrc1_comb;
+    wire        pcSrc2_comb;
+    wire [2:0]  wdSrc_comb;
+    wire        dmWe_comb;
+
+    wire        regWrite_fsm;
+    wire        select_comb_controls;
+
+    wire        is_crypt_instr;
+
+    control_comb control_comb (
+        .cmdOp        ( cmdOp         ),
+        .cmdF3        ( cmdF3         ),
+        .cmdF7        ( cmdF7         ),
+        .aluZero      ( aluZero       ),
+
+        .regWrite     ( regWrite_comb ),
+
+        .aluSrc1      ( aluSrc1       ),
+        .aluSrc2      ( aluSrc2       ),
+        .aluControl   ( aluControl    ),
+        .pcSrc1       ( pcSrc1_comb   ),
+        .pcSrc2       ( pcSrc2_comb   ),
+        .wdSrc        ( wdSrc_comb    ),
+
+        .dmWe         ( dmWe_comb     ),
+        .dmSign       ( dmSign        ),
+        .dmOpByte     ( dmOpByte      ),
+        .dmOpHalf     ( dmOpHalf      ),
+        .dmOpWord     ( dmOpWord      )
+    );
+
+    crypto_detector crypto_detector (
+        .opcode       ( cmdOp          ),
+        .func3        ( cmdF3          ),
+        .func7        ( cmdF7          ),
+        .func5_rs2    ( cmdF5_rs2      ),
+
+        .res          ( is_crypt_instr ),
+        .cryptMode    ( cryptMode      )
+    );
+
+    crypto_fsm crypto_fsm (
+        .clk               ( clk                  ),
+        .rst_n             ( rst_n                ),
+        .crypt_instr       ( is_crypt_instr       ),
+
+        .select_comb_ctrls ( select_comb_controls ),
+        .hold              ( hold                 ),
+        .regWrite          ( regWrite_fsm         )
+    );
+
+    always @(*) begin
+        if (select_comb_controls) begin
+            regWrite = regWrite_comb;
+            pcSrc1   = pcSrc1_comb;
+            pcSrc2   = pcSrc2_comb;
+            wdSrc    = wdSrc_comb;
+            dmWe     = dmWe_comb;
+        end else begin
+            regWrite = regWrite_fsm;
+            pcSrc1   = `PC_SRC1_4;
+            pcSrc2   = `PC_SRC2_PC;
+            wdSrc    = `WD_SRC_CRYPT;
+            dmWe     = 1'b0;
+        end
+    end
+
+endmodule
+
+
+module control_comb (
+    input      [ 6:0]  cmdOp,
+    input      [ 2:0]  cmdF3,
+    input      [ 6:0]  cmdF7,
+    input              aluZero,
+
+    output reg         regWrite,  // rf write enable
+
+    // multiplexers
+    output reg         aluSrc1,
+    output reg  [1:0]  aluSrc2,
+    output reg  [3:0]  aluControl,
+    output reg  [1:0]  pcSrc1,
+    output reg         pcSrc2,
+    output reg  [2:0]  wdSrc,
+
+    // memory
+    output reg         dmWe,      // data memory write enable
+    output reg         dmSign,    // data memory signed read
+    output             dmOpByte,   // data memory operation mode
+    output             dmOpHalf,
+    output             dmOpWord
+);
+
+    localparam DM_BYTE = 3'b001;
+    localparam DM_HALF = 3'b010;
+    localparam DM_WORD = 3'b100;
+
     wire         branch;
     reg          condZero;
     reg    [2:0] dmOpMode;
@@ -53,10 +143,6 @@ module sr_control
     always @ (*) begin
         condZero    = 1'b0;
         regWrite    = 1'b0;
-        // aluSrc      = 1'b0;
-        // wdSrc       = 1'b0;
-        // immPick     = 1'b0;
-        // memToReg    = 1'b0;
 
         wdSrc       = `WD_SRC_ALU;
         pcSrc1      = `PC_SRC1_4;
@@ -67,7 +153,7 @@ module sr_control
 
         dmWe        = 1'b0;
         dmSign      = 1'b0;
-        dmOpMode    = `DM_WORD;
+        dmOpMode    = DM_WORD;
 
         casez( { cmdF7, cmdF3, cmdOp } )
             { `RVF7_ADD,  `RVF3_ADD,  `RVOP_ADD   } : begin
@@ -200,7 +286,7 @@ module sr_control
                 aluSrc2 = `ALU_SRC2_IMMI;
                 wdSrc = `WD_SRC_MEM;
 
-                dmOpMode = `DM_BYTE;
+                dmOpMode = DM_BYTE;
                 dmSign = 1'b1;
             end
             { `RVF7_ANY,  `RVF3_LH,   `RVOP_LOAD  } : begin
@@ -208,7 +294,7 @@ module sr_control
                 aluSrc2 = `ALU_SRC2_IMMI;
                 wdSrc = `WD_SRC_MEM;
 
-                dmOpMode = `DM_HALF;
+                dmOpMode = DM_HALF;
                 dmSign = 1'b1;
             end
             { `RVF7_ANY,  `RVF3_LW,   `RVOP_LOAD  } : begin
@@ -216,7 +302,7 @@ module sr_control
                 aluSrc2 = `ALU_SRC2_IMMI;
                 wdSrc = `WD_SRC_MEM;
 
-                dmOpMode = `DM_WORD;
+                dmOpMode = DM_WORD;
                 dmSign = 1'b1;
             end
             { `RVF7_ANY,  `RVF3_LBU,  `RVOP_LOAD  } : begin
@@ -224,14 +310,14 @@ module sr_control
                 aluSrc2 = `ALU_SRC2_IMMI;
                 wdSrc = `WD_SRC_MEM;
 
-                dmOpMode = `DM_BYTE;
+                dmOpMode = DM_BYTE;
             end
             { `RVF7_ANY,  `RVF3_LHU,  `RVOP_LOAD  } : begin
                 regWrite = 1'b1;
                 aluSrc2 = `ALU_SRC2_IMMI;
                 wdSrc = `WD_SRC_MEM;
 
-                dmOpMode = `DM_HALF;
+                dmOpMode = DM_HALF;
             end
 
 
@@ -240,19 +326,19 @@ module sr_control
                 aluSrc2     = `ALU_SRC2_IMMS;
                 dmWe        = 1'b1;
 
-                dmOpMode    = `DM_BYTE;
+                dmOpMode    = DM_BYTE;
             end
             { `RVF7_ANY,  `RVF3_SH,   `RVOP_STORE } : begin
                 aluSrc2     = `ALU_SRC2_IMMS;
                 dmWe        = 1'b1;
 
-                dmOpMode    = `DM_HALF;
+                dmOpMode    = DM_HALF;
             end
             { `RVF7_ANY,  `RVF3_SW,   `RVOP_STORE } : begin
                 aluSrc2     = `ALU_SRC2_IMMS;
                 dmWe        = 1'b1;
 
-                dmOpMode    = `DM_WORD;
+                dmOpMode    = DM_WORD;
             end
 
 
@@ -310,19 +396,19 @@ module crypto_detector (
 
         casez ({ func5_rs2, func7, func3, opcode })
             { `RVF5_ANY,         `RVF7_AES32DSI,     `RVF3_AES,     `RVOP_AES    }: cryptMode = `CRYPT_MODE_VOID; // example
-            { `RVF5_ANY,         `RVF7_AES32DSMI,    `RVF3_AES,     `RVOP_AES    }: // TODO: и так далее
-            { `RVF5_ANY,         `RVF7_AES32ESI,     `RVF3_AES,     `RVOP_AES    }:
-            { `RVF5_ANY,         `RVF7_AES32ESMI,    `RVF3_AES,     `RVOP_AES    }:
-            { `RVF5_SHA256SIG0,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
-            { `RVF5_SHA256SIG1,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
-            { `RVF5_SHA256SUM0,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
-            { `RVF5_SHA256SUM1,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
-            { `RVF5_ANY,         `RVF7_SHA512SIG0H,  `RVF3_SHA512,  `RVOP_SHA512 }:
-            { `RVF5_ANY,         `RVF7_SHA512SIG0L,  `RVF3_SHA512,  `RVOP_SHA512 }:
-            { `RVF5_ANY,         `RVF7_SHA512SIG1H,  `RVF3_SHA512,  `RVOP_SHA512 }:
-            { `RVF5_ANY,         `RVF7_SHA512SIG1L,  `RVF3_SHA512,  `RVOP_SHA512 }:
-            { `RVF5_ANY,         `RVF7_SHA512SUM0R,  `RVF3_SHA512,  `RVOP_SHA512 }:
-            { `RVF5_ANY,         `RVF7_SHA512SUM1R,  `RVF3_SHA512,  `RVOP_SHA512 }:
+            // { `RVF5_ANY,         `RVF7_AES32DSMI,    `RVF3_AES,     `RVOP_AES    }: // TODO: и так далее
+            // { `RVF5_ANY,         `RVF7_AES32ESI,     `RVF3_AES,     `RVOP_AES    }:
+            // { `RVF5_ANY,         `RVF7_AES32ESMI,    `RVF3_AES,     `RVOP_AES    }:
+            // { `RVF5_SHA256SIG0,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
+            // { `RVF5_SHA256SIG1,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
+            // { `RVF5_SHA256SUM0,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
+            // { `RVF5_SHA256SUM1,  `RVF7_SHA256,       `RVF3_SHA256,  `RVOP_SHA256 }:
+            // { `RVF5_ANY,         `RVF7_SHA512SIG0H,  `RVF3_SHA512,  `RVOP_SHA512 }:
+            // { `RVF5_ANY,         `RVF7_SHA512SIG0L,  `RVF3_SHA512,  `RVOP_SHA512 }:
+            // { `RVF5_ANY,         `RVF7_SHA512SIG1H,  `RVF3_SHA512,  `RVOP_SHA512 }:
+            // { `RVF5_ANY,         `RVF7_SHA512SIG1L,  `RVF3_SHA512,  `RVOP_SHA512 }:
+            // { `RVF5_ANY,         `RVF7_SHA512SUM0R,  `RVF3_SHA512,  `RVOP_SHA512 }:
+            // { `RVF5_ANY,         `RVF7_SHA512SUM1R,  `RVF3_SHA512,  `RVOP_SHA512 }:
 
             default: begin
                 res = 1'b0;
@@ -339,7 +425,7 @@ module crypto_fsm (
     input       rst_n,
     input       crypt_instr,
 
-    output reg  ctrls_select,
+    output reg  select_comb_ctrls,
     output reg  hold,
     output reg  regWrite
 );
@@ -372,18 +458,18 @@ module crypto_fsm (
         case (next_state)
             S1: begin
                 hold = 1'b1;
-                ctrls_select = `SELECT_CRYP;
+                select_comb_ctrls = 1'b0;
                 regWrite = 1'b0;
             end
             S2: begin
                 hold = 1'b0;
-                ctrls_select = `SELECT_CRYP;
+                select_comb_ctrls = 1'b0;
                 regWrite = 1'b1;
             end
 
             default: begin
                 hold = 1'b0;
-                ctrls_select = `SELECT_COMB;
+                select_comb_ctrls = 1'b1;
                 regWrite = 1'b0;
             end
         endcase
