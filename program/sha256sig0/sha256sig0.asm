4925c946f8aa40f1db5a96853d76d6be5374f7d8
kdfrolov <139256075+kdfrolov@users.noreply.github.com>
2024-04-30 22:32:20 +0300

    Add sha256sig0 programs

---
+++ program/sha256sig0/sha256sig0.asm
@@ -0,0 +1,29 @@
+.text
+addi a1, zero, 5    # rs1
+
+addi a7, zero, 7
+jal ror             # ror32(inb, 7)
+add a2, zero, a0
+
+addi a7, zero, 18
+jal ror             # ror32(inb, 18)
+add a3, zero, a0
+
+srli a4, a1, 3 
+xor a0, a2, a3
+xor a0, a0, a4
+
+loop:
+   j loop
+  
+ror:
+   addi t0, zero, 31
+   and t0, t0, a7      # shamt = X(rs2)[4..0]
+   srl t2, a1, t0      # (X(rs1) >> shamt)
+   
+   addi t1, zero, 32   # xlen
+   sub t1, t1, t0      # (xlen - shamt)
+   sll t1, a1, t1      # (X(rs1) << (xlen - shamt))
+   
+   or a0, t2, t1
+   jr ra
---
+++ program/sha256sig0_original/sha256sig0_original.asm
@@ -0,0 +1,5 @@
+.text
+addi a1, zero, 5    # rs1
+#sha256sig0 a0, a1
+loop:
+   j loop
